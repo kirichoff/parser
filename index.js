c22f8fca740e01a9bf81a16d3d719b2ce3fa8119
kiril <kkd@omegasoftware.eu>
2020-01-04 18:36:07 +0300

    Many changes

--- index.js
+++ index.js
@@ -5,85 +5,91 @@ const scrapeIt = require("scrape-it");
 const base64Img = require('base64-img');
 let items = [];
 let categoriess = [{url: "/ru/products/trailers/"},{url: "/ru/products/passenger_vehicle/"}];
+let tick = 0;
 
-scrapeIt(urlProducts, {
-    categories: {
-        listItem: ".b-menu_left li",
-        name: 'categories',
-        data:{
-            title: 'a',
-            url:{selector: 'a',attr: 'href'}
+async function  f(){
+   let categoryes = await scrapeIt(urlProducts, {
+        categories: {
+            listItem: ".b-menu_left li",
+            name: 'categories',
+            data:{
+                title: 'a',
+                url:{selector: 'a',attr: 'href'}
+            }
         }
-    }
-}).catch((er)=>console.log('Er0',er))
-    .then(({data,response})=> {
-        console.log('data 1',data);
-
-        new Promise ((resolve, reject)=>{
-
-        for(let pt of data.categories){
-            const encoded = encodeURI(pt.url);
-            console.log(encoded);
-            scrapeIt(url+encoded,{
-                items:{
-                listItem: '.art-list li',
-                name: 'arts',
-                data: {
-                    title: 'a',
-                    url: {selector: 'a', attr: 'href'}
-                }
-                }
-            }).then(({data,response})=>{
-                categoriess = [...categoriess,...data.items]
-                if (categoriess.length>=29)
-                    resolve();
-                console.log(categoriess)
-                }
-            );
-        }
-        }).then(() => {
-            console.log('New start')
-            let categories = categoriess;
-            for(let category of  categories){
-                const encoded = encodeURI(category.url);
-                scrapeIt(url+encoded, {
-                    itemsByTabs: {
-                        listItem: '.tabs div .b-catalog__item',
-                        name: 'tab',
-                        categories: 'h1',
+    });
+    categoryes = categoryes.data.categories;
+            for(let pt of categoryes){
+                    const encoded = encodeURI(pt.url);
+                    console.log(encoded);
+                     let promise = await   scrapeIt(url+encoded,{
+                    items:{
+                        listItem: '.art-list li',
+                        name: 'arts',
                         data: {
-                            title: '.b-cit__item a',
-                            url: {selector: 'a',attr: 'href'},
+                            title: 'a',
+                            url: {selector: 'a', attr: 'href'}
                         }
                     }
-                }).catch((er)=>console.log('Er1',er)).then(({data,response}) =>{
-                    let itemsByTabs = [...data.itemsByTabs];
-                    for(let item of itemsByTabs){
-                        const encoded = encodeURI(item.url);
-                        console.log(`${url}/${encoded}`);
-                        scrapeIt(url+encoded,{
-                            title: 'h1',
-                            image: {selector:'.b-c__pic img', attr:'src'},
-                            description: '.b-catalog p',
-                            category: '.crumbs a:nth-child(5n)',
-                            features: {
-                                listItem: 'table tr',
-                                data: {
-                                    text: 'td'
-                                }
+                });
+                    categoriess = [...categoriess, ... promise.data.items];
+                }
+                            let categories = categoriess;
+                            let itemsByTabs = [];
+                            for(let category of  categories) {
+                                const encoded = encodeURI(category.url);
+                                let result = await scrapeIt(url + encoded, {
+                                    itemsByTabs: {
+                                        listItem: '.tabs div .b-catalog__item',
+                                        name: 'tab',
+                                        categories: 'h1',
+                                        data: {
+                                            title: '.b-cit__item a',
+                                            url: {selector: 'a', attr: 'href'},
+                                        }
+                                    }
+                                });
+                                itemsByTabs  = [...itemsByTabs,...result.data.itemsByTabs];
+                                console.log(itemsByTabs.length)
                             }
-                        }).catch((er)=>console.log('Er2',er)).then(({data,response})=>{
-                            //console.log('data3',data)
-                            if(data){
+                            console.log(itemsByTabs);
+                            let dataItems = [];
+                            for(let item of itemsByTabs){
+                                const encoded = encodeURI(item.url);
+                                //console.log(`${url}/${encoded}`);
+                                let result = await scrapeIt(url+encoded,{
+                                    title: 'h1',
+                                    image: {selector:'.b-c__pic img', attr:'src'},
+                                    description: '.b-catalog p',
+                                    category: '.crumbs a:nth-child(5n)',
+                                    features: {
+                                        listItem: 'table tr',
+                                        data: {
+                                            text: 'td'
+                                        }
+                                    }
+                                });
+                                dataItems.push(result.data);
+                                console.log(dataItems.length)
+                            }
+                            console.log('dataitems',dataItems.length);
+                            for(let data of dataItems) {
                                 const encoded = encodeURI(data.image);
-                                base64Img.requestBase64(url+encoded,function (err,res,body) {
-                                    items.push({...data, image: body});
-                                    console.log(items)
-                                });}
-                        }).catch((er)=>console.log('Er3',er))
-                    }
-                })
+                                await new Promise( (resolve,reject) => {
+                                    base64Img.requestBase64(url + encoded, function (err, res, body) {
+                                        items.push({...data, image: body});
+                                        resolve({data: body});
+                                        console.log('items', items.length);
+                                    })});
+                            }
+                            return items;
             }
-        });
-    });
-
+             f().then(k=>console.log('KKK',k));
+let inter = setInterval(function () {
+    if(items.length>=314) {
+        console.log(items);
+        console.log('Tick',tick / 10);
+        // clearInterval(inter)
+    }
+    tick++;
+},100);
